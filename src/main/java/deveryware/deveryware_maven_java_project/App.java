febf9858d01574a3cf173a89e9351145c42dd86f
Olivier Moreau <olivier.moreau@deveryware.com>
2015-11-30 15:21:05 +0100

    Init

---
+++ src/main/java/deveryware/deveryware_maven_java_project/App.java
@@ -0,0 +1,13 @@
+package deveryware.deveryware_maven_java_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
